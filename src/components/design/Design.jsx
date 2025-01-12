d2d22123e05473653ac893c41bb3094459a80e88
motfisi <108813531+motfisi@users.noreply.github.com>
2025-01-13 02:08:56 +0300

    design

--- src/components/design/Design.jsx
+++ src/components/design/Design.jsx
@@ -22,6 +22,49 @@ const Design = () => {
           </div>
         </div>
       </div>
+      <div className="design__cards-container">
+        <div className="design__card">
+          <div className="design__card__img design__card__img--1"></div>
+          <div className="design__card__text1">Очистка барабана</div>
+          <div className="design__card__text2">
+            Поддержание внутренних частей стиральной машины в чистоте,
+            предотвращает появление неприятных запахов.
+          </div>
+        </div>
+        <div className="design__card">
+          <div className="design__card__img design__card__img--2"></div>
+          <div className="design__card__text1">Программа «Любимая»</div>
+          <div className="design__card__text2">
+            Позволяет записывать и сохранять в памяти стиральной машины
+            понравившуюся программу.
+          </div>
+        </div>
+        <div className="design__card">
+          <div className="design__card__img design__card__img--3"></div>
+          <div className="design__card__text1">
+            Энергоэффективность класса А+++
+          </div>
+          <div className="design__card__text2">
+            Экономит электроэнергию и оплату счетов за электричество.
+          </div>
+        </div>
+        <div className="design__card">
+          <div className="design__card__img design__card__img--4"></div>
+          <div className="design__card__text1">Защита от детей</div>
+          <div className="design__card__text2">
+            Активировав опцию, кнопки управления блокируются, поэтому дети не
+            смогут вмешиваться в ход стирки.
+          </div>
+        </div>
+        <div className="design__card">
+          <div className="design__card__img design__card__img--5"></div>
+          <div className="design__card__text1">Smart Balance Control</div>
+          <div className="design__card__text2">
+            Система электронного контроля дисбаланса обеспечивает стабильную
+            работу машины без повышенных вибраций и шума.
+          </div>
+        </div>
+      </div>
     </>
   );
 };
